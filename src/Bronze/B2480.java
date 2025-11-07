c7f7d575eab3d15010b369e705d37f1549227b08
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-07 22:48:51 +0900

    1107

---
+++ src/Bronze/B2480.java
@@ -0,0 +1,12 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B2480 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        
+
+    }
+}
--- src/Bronze/B2525.java
+++ src/Bronze/B2525.java
@@ -1,7 +1,21 @@
 package Bronze;
 
-public class B2525 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class B2525 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = in.readLine().split(" ");
+        int X = Integer.parseInt(split[0]);
+        int Y = Integer.parseInt(split[1]);
+
+        int T = Integer.parseInt(in.readLine());
+
+
+        X = (X + (Y + T) / 60) % 24;
+        Y = (Y + T) % 60;
+
+        System.out.println(X + " " + Y);
     }
 }
