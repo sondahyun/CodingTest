f986615a27fd4ac58d4aff66d0416e0fa27f8946
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-08 00:27:02 +0900

    1107

--- src/Bronze/B10951.java
+++ src/Bronze/B10951.java
@@ -3,12 +3,13 @@ package Bronze;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+// 종료 조건이 없을 때
 public class B10951 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < 5; i++) {
-            String[] split = in.readLine().split(" ");
+        String line;
+        while ((line = in.readLine()) != null) {
+            String[] split = line.split(" ");
             int A = Integer.parseInt(split[0]);
             int B = Integer.parseInt(split[1]);
             System.out.println(A + B);
