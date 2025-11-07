c6e0542cc804e69d258a01f4b795d9664fb1c602
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-07 22:38:09 +0900

    1107

---
+++ src/Bronze/B2525.java
@@ -0,0 +1,7 @@
+package Bronze;
+
+public class B2525 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Bronze/B2884.java
+++ src/Bronze/B2884.java
@@ -1,4 +1,24 @@
 package Bronze;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class B2884 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = in.readLine().split(" ");
+        int A = Integer.parseInt(split[0]);
+        int B = Integer.parseInt(split[1]);
+
+        if ( B >= 45) {
+            System.out.println(A + " " + (B-45));
+        } else {
+            if (A >= 1) {
+                System.out.println((A - 1) + " " + (60 - 45 + B));
+            } else {
+                System.out.println((24 - 1 + A) + " " + (60 - 45 + B));
+            }
+        }
+
+    }
 }
