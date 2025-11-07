36d20398c95e0441da81c80cf84f124e7b33d0ba
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-08 00:32:42 +0900

    1107

---
+++ src/Bronze/B11021.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// 종료 조건이 없을 때
+public class B11021 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(in.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            String[] split = in.readLine().split(" ");
+            int A = Integer.parseInt(split[0]);
+            int B = Integer.parseInt(split[1]);
+            int count = A + B;
+            System.out.println("Case #"+i+ ": " + (A+B));
+        }
+
+    }
+}
