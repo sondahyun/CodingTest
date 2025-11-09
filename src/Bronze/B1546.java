2c3ee861573d1b31790a46bd2c49d7211a41405e
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-09 13:07:45 +0900

    1109

--- src/Bronze/B1546.java
+++ src/Bronze/B1546.java
@@ -3,7 +3,7 @@ package Bronze;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class B10811 {
+public class B1546 {
     public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
