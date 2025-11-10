9eeead5a6dae033142ba96544d8e7ad30089dd0f
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-10 20:29:40 +0900

    1110

--- src/Silver/S2941.java
+++ src/Silver/S2941.java
@@ -2,8 +2,7 @@ package Silver;
 
 public class S2941 {
     public static void main(String[] args) {
-        System.out.println((int)'a');
-        System.out.println((int)'z');
+        
 
     }
 }
