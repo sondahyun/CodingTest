d662736705db5632608868394f20bcc90b220a74
DAHYUN <88223753+sondahyun@users.noreply.github.com>
2025-11-20 23:57:34 +0900

    S2941.java 업데이트

--- src/Silver/S2941.java
+++ src/Silver/S2941.java
@@ -3,6 +3,7 @@ package Silver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+
 public class S2941 {
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
